9bc2e3fc49b73d57a6de517b3e1c68a3868780de
alextaves <42424246+alextaves@users.noreply.github.com>
2019-03-08 13:36:26 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>hey</div>;
+  }
+}
+
+export default App;
